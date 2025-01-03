cd95980a87e533625f9e9173de80baa36e2e0e06
Angel Pusch <eangel00302@gmail.com>
2025-01-03 00:08:39 -0300

    🤕 fighting

--- scripts/splash_screen.gd
+++ scripts/splash_screen.gd
@@ -3,5 +3,11 @@ extends Control
 func _ready() -> void:
     $Timer.start()
 
-func _on_Timer_timeout() -> void:
-    get_tree().change_scene("res://scenes/main.tscn")
+func _on_timer_timeout() -> void:
+    # Fade animation
+    $AnimationPlayer.play("fade_out")
+    $AnimationPlayer.connect("animation_finished", Callable(self, "_on_animation_finished"))
+
+func _on_animation_finished(anim_name: String) -> void:
+    if anim_name == "fade_out":
+        get_tree().change_scene_to_file("res://scenes/main.tscn")
